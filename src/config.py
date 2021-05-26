bdac8d3e1743ec52d686601c86dda7d5ad90e011
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-05-26 01:09:09 -0300

    Dejo hasta aca por hoy

--- src/config.py
+++ src/config.py
@@ -6,6 +6,6 @@ config = {
     "alto_imagenes": 64,
     "ancho_imagenes_a_procesar": 128,
     "alto_imagenes_a_procesar": 128,
-    "epochs": 200,
+    "epochs": 3,
     "tasa_aprendizaje": 0.001,
 }
